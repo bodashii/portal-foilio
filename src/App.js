259a41ff87a48c03be1a668060d10c17d163baf1
maddnty <nzkb24@gmail.com>
2022-05-07 23:16:01 -0500

    added about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <h1>Nicolas Young</h1>
       <Nav />
+      <About />
     </div>
   );
 }
