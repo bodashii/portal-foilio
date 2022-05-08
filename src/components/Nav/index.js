86c25626249c1e26aaf08663de6fd7aa9049efa5
maddnty <nzkb24@gmail.com>
2022-05-08 12:42:56 -0500

    updated style on nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,8 +4,13 @@ function Nav () {
 
     return (
         <header className="flex-row px-1">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <span className="navbar-brand">Nicolas Young</span>
+            <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-light">
+                <span className="navbar-brand px-2">
+                    <span className="nav-item" aria-label="web dev" role="img">💻 <span>Nicolas Young</span>
+                        
+                    </span>
+                </span>
+
                 <div className="container-fluid">
                     <ul className="navbar-nav">
                         <li className="nav-item">
