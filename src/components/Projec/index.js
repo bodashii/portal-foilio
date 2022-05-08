4952e9fd95002e2e1376684e7a7569478918082d
maddnty <nzkb24@gmail.com>
2022-05-08 12:58:01 -0500

    updated style to projects component

--- src/components/Projec/index.js
+++ src/components/Projec/index.js
@@ -4,38 +4,42 @@ function Projec () {
     return (
         <section className="container">
             <h3>Projects</h3>
-            <div>
-                <div>
+            <div className="row">
+                <div className="col">
                     <a href="./project1">Project 1
                         <img alt="project1"/>
                     </a>
                 </div>
 
-                <div>
+                <div className ="col">
                     <a href="./project2">Project 2
                         <img alt="project2" />
                     </a>
                 </div>
+            </div>
 
-                <div>
+            <div className ="row">
+                <div className="col">
                     <a href="./project3">Project 3
                         <img alt="project3"/>
                     </a>
                 </div>
 
-                <div>
+                <div className="col">
                     <a href="./project4">Project 4
                         <img alt="project4" />
                     </a>
                 </div>
-
-                <div>
+            </div>
+            
+            <div className="row">
+                <div className="col">
                     <a href="./project5">Project 5
                         <img alt="project5" />
                     </a>
                 </div>
 
-                <div>
+                <div className="col">
                     <a href="./project6">Project 6
                         <img alt="project6"/>
                     </a>
